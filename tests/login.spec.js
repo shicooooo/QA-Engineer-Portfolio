5ec624db04cff5b393e9c6371202e07ee3ad2eb7
sherif <shicosamy402@gmail.com>
2026-06-03 18:36:53 +0300

    add login tests: valid and invalid credentials

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -7,3 +7,11 @@ test ('login with valid credentials', async ({page})=> {
     await page.click('button[type="submit"]');
     await expect(page.locator('.flash.success')).toBeVisible();
 });
+
+test('login with invalid credentials', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+    await page.fill('#username', 'wronguser');
+    await page.fill('#password', 'wrongpassword');
+    await page.click('button[type="submit"]');
+    await expect(page.locator('.flash.error')).toBeVisible({timeout:10000});
+})
