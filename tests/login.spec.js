7abf53b83fc07bef3f9f879f8141f592251a4a51
sherif <shicosamy402@gmail.com>
2026-06-06 17:52:39 +0300

    refactor: add beforeEach hook

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,4 +1,4 @@
-    const {test,expect}=require("@playwright/test")
+    /*const {test,expect}=require("@playwright/test")
     const { LoginPage } = require('./pages/LoginPage');
     
     test("login with valid credentials",async({page})=>{
@@ -20,4 +20,29 @@
         await loginPage.goto();
         await loginPage.login('','');
         await expect(loginPage.errorMessage).toBeVisible({timeout:10000})
+    })
+    */
+
+    const {test,expect}=require("@playwright/test")
+    const { LoginPage }=require('./pages/LoginPage');
+
+    let loginPage;
+    test.beforeEach(async({page})=>{
+        loginPage=new LoginPage(page);
+        await loginPage.goto();
+    });
+
+    test ("login with valid credentials",async({page})=>{
+        await loginPage.login('tomsmith','SuperSecretPassword!');
+        await expect(loginPage.successMessage).toBeVisible();
+    });
+    
+    test("login with invalid credentials",async({page})=>{
+        await loginPage.login('invalidUser','invalidPassword');
+        await expect(loginPage.errorMessage).toBeVisible({timeout:10000});
+    });
+
+    test("login with empty credentials",async({page})=>{
+        await loginPage.login('','');
+        await expect(loginPage.errorMessage).toBeVisible({timeout:10000});
     })
