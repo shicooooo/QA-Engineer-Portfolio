88819aa55cc47391584decb6cf5130c59cd3effd
sherif <shicosamy402@gmail.com>
2026-06-07 21:18:15 +0300

    add data-driven testing

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -49,3 +49,16 @@
         await expect(loginPage.errorMessage).toBeVisible({timeout:10000});
         await expect(page).toHaveURL('https://the-internet.herokuapp.com/login');
     })
+
+    const loginData=[
+    {username:'tomsmith',password:'SuperSecretPassword!',expectedResult:'success'},
+    {username:'wrongUser',password:'wrongPassword',expectedResult:'error'},
+    {username:'',password:'',expectedResult:'error'}
+    ];
+
+    for (const data of loginData){
+        test(`login test ${data.username ||'empty' }credentials`,async({page})=>{
+        const loginPage=new LoginPage(page);
+        await loginPage.goto();
+        await loginPage.login(data.username,data.password);
+        })}
