bd1c96ee5af1956cb3c98b28f58878698851022e
sherif <shicosamy402@gmail.com>
2026-06-12 03:24:12 +0300

    fix: correct loginPage import case sensitivity

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -24,7 +24,7 @@
     */
 
     const {test,expect}=require("@playwright/test")
-    const { LoginPage }=require('./pages/LoginPage');
+    const { LoginPage }=require('./pages/loginPage');
 
     /*
     OLD WAY
